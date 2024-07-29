53c67da2cfe2f0a9ab3b4c5f971e057effb1a40c
Austin <austinngugi000@gmail.com>
2024-07-29 18:49:18 +0300

    initial commit

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
-  def __init__(self, discount=0):
+    def __init__(self, discount=0):
         self.discount = discount
         self.total = 0
         self.items = []
